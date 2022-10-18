d24c90b716f8240e9c4d6e7725300cdbd5abec07
Ubong Isaiah <114228650+fezcap@users.noreply.github.com>
2022-10-18 05:35:07 +0100

    Add files via upload

---
+++ home/index.js
@@ -0,0 +1,35 @@
+  (function (){
+    
+    function body(id){
+      return `
+        
+      `;
+    }
+    
+    
+    
+    function script(){ }
+    function css(){ }
+    
+    
+    fc.buildPage.main = {
+      body: body,
+      
+      script: script,
+      css: css,
+      
+      extScript: [],
+      extCss: [],
+      
+      header: true,
+      footer: true,
+      
+      quickLink: 'home',
+      
+      description: '',
+      title: 'home',
+      
+      elemId: 'm '
+      
+    };
+  }());
